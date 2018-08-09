b9818171c5a4b720c09d2b0a88004ffeaf80c94b
Nico van Duijn <nico@auterion.com>
2018-08-09 16:26:23 +0200

    Formatting

--- bug_planner/include/bug_planner.hpp
+++ bug_planner/include/bug_planner.hpp
@@ -1,7 +1,6 @@
 #ifndef BUG_PLANNER_H
 #define BUG_PLANNER_H
 
-
 #include <mavros_msgs/Trajectory.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <ros/ros.h>
@@ -9,16 +8,15 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/transforms.h>
 
-class BugPlanner {
- public:
-
+class BugPlanner
+{
+public:
   BugPlanner();
   ~BugPlanner();
 
- private:
+private:
   ros::NodeHandle nh_;
- 
-  
+
   ros::Subscriber fcu_input_sub_;
   ros::Subscriber depth_cam_sub_;
   ros::Publisher mavros_obstacle_free_path_pub_;
@@ -29,17 +27,15 @@ class BugPlanner {
   geometry_msgs::PoseStamped goal_pose_;
   const float CLOSEST_OBSTACLE_THRESHOLD = 3.0;
   const float YAW_RATE = 0.5;
-  const float OBS_TIME_THR = 2.0; 
+  const float OBS_TIME_THR = 2.0;
 
   ros::Time time_of_last_obstacle_;
   bool obstacle_in_sight_;
- 
+
   void fcuInputGoalCallback(const mavros_msgs::Trajectory& msg);
   void fillUnusedTrajectoryPoint(mavros_msgs::PositionTarget& point);
-  void avoid(mavros_msgs::Trajectory& obst_avoid,
-                                        const mavros_msgs::Trajectory& msg);
+  void avoid(mavros_msgs::Trajectory& obst_avoid, const mavros_msgs::Trajectory& msg);
   void depthCameraCallback(const sensor_msgs::PointCloud2& msg);
 };
-
 
 #endif  // BUG_PLANNER_H
--- bug_planner/src/bug_planner.cpp
+++ bug_planner/src/bug_planner.cpp
@@ -1,56 +1,53 @@
 #include "bug_planner.hpp"
 
-BugPlanner::BugPlanner():time_of_last_obstacle_(ros::Time::now()), obstacle_in_sight_(false) {
+BugPlanner::BugPlanner() : time_of_last_obstacle_(ros::Time::now()), obstacle_in_sight_(false)
+{
   nh_ = ros::NodeHandle("~");
 
-  fcu_input_sub_ =
-      nh_.subscribe("/mavros/trajectory/desired", 1,
-                    &BugPlanner::fcuInputGoalCallback, this);
+  fcu_input_sub_ = nh_.subscribe("/mavros/trajectory/desired", 1, &BugPlanner::fcuInputGoalCallback, this);
   depth_cam_sub_ = nh_.subscribe("/camera/depth/points", 1, &BugPlanner::depthCameraCallback, this);
-  mavros_obstacle_free_path_pub_ = nh_.advertise<mavros_msgs::Trajectory>(
-      "/mavros/trajectory/generated", 10);
+  mavros_obstacle_free_path_pub_ = nh_.advertise<mavros_msgs::Trajectory>("/mavros/trajectory/generated", 10);
   current_pos_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("/current_pos", 10);
-  current_goal_pub_ =
-      nh_.advertise<geometry_msgs::PoseStamped>("/goal_pos", 10);
+  current_goal_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("/goal_pos", 10);
 }
 
-BugPlanner::~BugPlanner() {};
+BugPlanner::~BugPlanner(){};
 
-void BugPlanner::fcuInputGoalCallback(
-    const mavros_msgs::Trajectory& msg) {
- 
-  mavros_msgs::Trajectory adjusted_traj_ = {};
-  avoid(adjusted_traj_, msg);
-  mavros_obstacle_free_path_pub_.publish(adjusted_traj_);
-
+void BugPlanner::fcuInputGoalCallback(const mavros_msgs::Trajectory& msg)
+{
+  // Save the current pose
+  tf::Quaternion q;
+  q.setRPY(0.0, 0.0, msg.point_1.yaw);
   current_pose_.header = msg.header;
   current_pose_.pose.position.x = msg.point_1.position.x;
   current_pose_.pose.position.y = msg.point_1.position.y;
   current_pose_.pose.position.z = msg.point_1.position.z;
-
-  tf::Quaternion q;
-  q.setRPY(0.0, 0.0, msg.point_1.yaw);
   current_pose_.pose.orientation.w = q.getW();
   current_pose_.pose.orientation.x = q.getX();
   current_pose_.pose.orientation.y = q.getY();
   current_pose_.pose.orientation.z = q.getZ();
 
+  // Save the goal pose
+  q.setRPY(0.0, 0.0, msg.point_2.yaw);
   goal_pose_.header = msg.header;
   goal_pose_.pose.position.x = msg.point_2.position.x;
   goal_pose_.pose.position.y = msg.point_2.position.y;
   goal_pose_.pose.position.z = msg.point_2.position.z;
-  q.setRPY(0.0, 0.0, msg.point_2.yaw);
   goal_pose_.pose.orientation.w = q.getW();
   goal_pose_.pose.orientation.x = q.getX();
   goal_pose_.pose.orientation.y = q.getY();
   goal_pose_.pose.orientation.z = q.getZ();
 
+  mavros_msgs::Trajectory adjusted_traj_ = {};
+  avoid(adjusted_traj_, msg);
+
+  mavros_obstacle_free_path_pub_.publish(adjusted_traj_);
   current_pos_pub_.publish(current_pose_);
   current_goal_pub_.publish(goal_pose_);
 }
 
-void BugPlanner::fillUnusedTrajectoryPoint(
-    mavros_msgs::PositionTarget& point) {
+void BugPlanner::fillUnusedTrajectoryPoint(mavros_msgs::PositionTarget& point)
+{
   point.position.x = NAN;
   point.position.y = NAN;
   point.position.z = NAN;
@@ -64,8 +61,8 @@ void BugPlanner::fillUnusedTrajectoryPoint(
   point.yaw_rate = NAN;
 }
 
-void BugPlanner::avoid(
-    mavros_msgs::Trajectory& obst_avoid, const mavros_msgs::Trajectory& msg) {
+void BugPlanner::avoid(mavros_msgs::Trajectory& obst_avoid, const mavros_msgs::Trajectory& msg)
+{
   obst_avoid.header = msg.header;
   obst_avoid.type = 0;
   obst_avoid.point_1.position.x = NAN;
@@ -77,7 +74,8 @@ void BugPlanner::avoid(
   obst_avoid.point_1.acceleration_or_force.z = NAN;
 
   // if an obstacle is ahead, just yaw and find a "free direction"
-  if(obstacle_in_sight_){
+  if (obstacle_in_sight_)
+  {
     obst_avoid.point_1.velocity.x = 0.0f;
     obst_avoid.point_1.velocity.y = 0.0f;
     obst_avoid.point_1.velocity.z = 0.0f;
@@ -85,26 +83,30 @@ void BugPlanner::avoid(
   }
 
   // if there is no obstacle ahead, but we have recently had one, go straight
-  else if(ros::Time::now().toSec() - time_of_last_obstacle_.toSec() < OBS_TIME_THR){
-    obst_avoid.point_1.velocity.x = cos(msg.point_1.yaw)*1.5;
-    obst_avoid.point_1.velocity.y = sin(msg.point_1.yaw)*1.5;
+  else if (ros::Time::now().toSec() - time_of_last_obstacle_.toSec() < OBS_TIME_THR)
+  {
+    obst_avoid.point_1.velocity.x = cos(msg.point_1.yaw) * 1.5;
+    obst_avoid.point_1.velocity.y = sin(msg.point_1.yaw) * 1.5;
     obst_avoid.point_1.velocity.z = 0.0f;
     obst_avoid.point_1.yaw_rate = 0.0f;
-}
-// if there's no obstacle ahead, but we're not facing the goal, just yaw and/or climb/descend    
-else if(std::fabs(tf::getYaw(current_pose_.pose.orientation)-
-                            (atan2(goal_pose_.pose.position.y - current_pose_.pose.position.y, 
-                                   goal_pose_.pose.position.x - current_pose_.pose.position.x)))
-  > 0.1){
+  }
+
+  // if there's no obstacle ahead, but we're not facing the goal, just yaw and/or climb/descend
+  else if (std::fabs(tf::getYaw(current_pose_.pose.orientation) -
+                     (atan2(goal_pose_.pose.position.y - current_pose_.pose.position.y,
+                            goal_pose_.pose.position.x - current_pose_.pose.position.x))) > 0.1)
+  {
     obst_avoid.point_1.velocity.x = 0.0f;
     obst_avoid.point_1.velocity.y = 0.0f;
     obst_avoid.point_1.velocity.z = msg.point_1.velocity.z;
     obst_avoid.point_1.yaw_rate = msg.point_1.yaw_rate;
+  }
 
-// if there is no obstacle ahead, and it's been a while that there was, we can fly toward the goal
-  }else{
-    obst_avoid.point_1.velocity.x = msg.point_1.velocity.x*0.3;
-    obst_avoid.point_1.velocity.y = msg.point_1.velocity.y*0.3;
+  // if there is no obstacle ahead, and it's been a while that there was, we can fly toward the goal
+  else
+  {
+    obst_avoid.point_1.velocity.x = msg.point_1.velocity.x * 0.3;
+    obst_avoid.point_1.velocity.y = msg.point_1.velocity.y * 0.3;
     obst_avoid.point_1.velocity.z = msg.point_1.velocity.z;
     obst_avoid.point_1.yaw_rate = msg.point_1.yaw_rate;
   }
@@ -113,36 +115,40 @@ else if(std::fabs(tf::getYaw(current_pose_.pose.orientation)-
   fillUnusedTrajectoryPoint(obst_avoid.point_3);
   fillUnusedTrajectoryPoint(obst_avoid.point_4);
   fillUnusedTrajectoryPoint(obst_avoid.point_5);
-  obst_avoid.time_horizon = {NAN, NAN, NAN, NAN, NAN};
-  obst_avoid.point_valid = {true, false, false, false, false};
+  obst_avoid.time_horizon = { NAN, NAN, NAN, NAN, NAN };
+  obst_avoid.point_valid = { true, false, false, false, false };
 }
 
-  void BugPlanner::depthCameraCallback(const sensor_msgs::PointCloud2& msg){
-     pcl::PointCloud<pcl::PointXYZ> cloud;  // Easier to loop through pcl::PointCloud
-    pcl::fromROSMsg(msg, cloud);
+void BugPlanner::depthCameraCallback(const sensor_msgs::PointCloud2& msg)
+{
+  pcl::PointCloud<pcl::PointXYZ> cloud;  // Easier to loop through pcl::PointCloud
+  pcl::fromROSMsg(msg, cloud);
 
-    // Look through the point cloud, only if we have reached a height higher than our 
-    // CLOSEST_OBSTACLE_THRESHOLD since otherwise the ground would trigger an obstacle
-    if(current_pose_.pose.position.z > CLOSEST_OBSTACLE_THRESHOLD){
-      for (const auto& p : cloud) {
-        if (!std::isnan(p.x) && !std::isnan(p.y) && !std::isnan(p.z)) {
+  // Look through the point cloud, only if we have reached a height higher than our
+  // CLOSEST_OBSTACLE_THRESHOLD since otherwise the ground would trigger an obstacle
+  if (current_pose_.pose.position.z > CLOSEST_OBSTACLE_THRESHOLD)
+  {
+    for (const auto& p : cloud)
+    {
+      if (!std::isnan(p.x) && !std::isnan(p.y) && !std::isnan(p.z))
+      {
         // points in camera frame -> if p.y is close to zero it is roughly on the same height as the drone
-         if(p.y > -0.5 && 
-            p.y <  0.5 &&
-          (p.x*p.x + p.y*p.y + p.z*p.z) < CLOSEST_OBSTACLE_THRESHOLD*CLOSEST_OBSTACLE_THRESHOLD){
-            time_of_last_obstacle_ = ros::Time::now();
-            obstacle_in_sight_ = true;
-            return;
-          }
+        if (p.y > -0.5 && p.y < 0.5 &&
+            (p.x * p.x + p.y * p.y + p.z * p.z) < CLOSEST_OBSTACLE_THRESHOLD * CLOSEST_OBSTACLE_THRESHOLD)
+        {
+          time_of_last_obstacle_ = ros::Time::now();
+          obstacle_in_sight_ = true;
+          return;
         }
       }
     }
-    obstacle_in_sight_ = false;
-    return;
   }
+  obstacle_in_sight_ = false;
+  return;
+}
 
-
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "bug_planner");
   BugPlanner planner;
 
