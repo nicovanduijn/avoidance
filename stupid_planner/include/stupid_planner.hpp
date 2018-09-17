41d86baf495686a23d9b0159a79390a83240cbf2
Nico van Duijn <nico@auterion.com>
2018-09-17 14:21:30 +0200

    Fix stupid planner for FlightTask
    This changes the way the stupid planner works, since now
    the firmware no longer publishes the desired trajectory at 5Hz
    but only sends it once if the triplet updates.

--- stupid_planner/include/stupid_planner.hpp
+++ stupid_planner/include/stupid_planner.hpp
@@ -4,28 +4,43 @@
 
 #include <mavros_msgs/Trajectory.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TwistStamped.h>
 #include <ros/ros.h>
 
 class StupidPlanner {
  public:
+	// This planner utilises the carrot-and-stick method to trick the
+	// position controller to follow a trajectory toward the goal. The
+	// CARROT_STICK_LENGTH sort-of defines the speed
+	const float CARROT_STICK_LENGTH = 2.0;
 
   StupidPlanner();
   ~StupidPlanner();
+
+  void publishOutput();
 
  private:
   ros::NodeHandle nh_;
  
   
   ros::Subscriber fcu_input_sub_;
+  ros::Subscriber pose_sub_;
+  ros::Subscriber vel_sub_;
   ros::Publisher mavros_obstacle_free_path_pub_;
-  ros::Publisher current_pos_pub_;
-  ros::Publisher current_goal_pub_;
+  ros::Publisher current_pose_pub_;
+  ros::Publisher goal_pose_pub_;
 
- 
+  geometry_msgs::PoseStamped current_pose_;
+  geometry_msgs::TwistStamped current_vel_;
+  geometry_msgs::PoseStamped goal_pose_;
+  mavros_msgs::Trajectory obst_free_path_;
+
+
   void fcuInputGoalCallback(const mavros_msgs::Trajectory& msg);
+  void velCallback(const geometry_msgs::TwistStamped& msg);
+  void poseCallback(const geometry_msgs::PoseStamped& msg);
   void fillUnusedTrajectoryPoint(mavros_msgs::PositionTarget& point);
-  void transformPoseToObstacleAvoidance(mavros_msgs::Trajectory& obst_avoid,
-                                        const mavros_msgs::Trajectory& msg);
+
 };
 
 
--- stupid_planner/src/stupid_planner.cpp
+++ stupid_planner/src/stupid_planner.cpp
@@ -1,5 +1,6 @@
 #include "stupid_planner.hpp"
-//#define POSITION_SETPOINTS
+#define POSITION_SETPOINTS
+#define VERBOSE
 
 StupidPlanner::StupidPlanner() {
   nh_ = ros::NodeHandle("~");
@@ -7,53 +8,116 @@ StupidPlanner::StupidPlanner() {
   fcu_input_sub_ =
       nh_.subscribe("/mavros/trajectory/desired", 1,
                     &StupidPlanner::fcuInputGoalCallback, this);
+  pose_sub_ = nh_.subscribe<const geometry_msgs::PoseStamped&>(
+        "/mavros/local_position/pose", 1, &StupidPlanner::poseCallback,
+        this);
+  vel_sub_ = nh_.subscribe<const geometry_msgs::TwistStamped&>("/mavros/local_position/velocity", 1,
+                                  &StupidPlanner::velCallback, this);
 
   mavros_obstacle_free_path_pub_ = nh_.advertise<mavros_msgs::Trajectory>(
       "/mavros/trajectory/generated", 10);
-  current_pos_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("/current_pos", 10);
-  current_goal_pub_ =
-      nh_.advertise<geometry_msgs::PoseStamped>("/goal_pos", 10);
+  current_pose_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("/current_pose", 10);
+  goal_pose_pub_ =
+      nh_.advertise<geometry_msgs::PoseStamped>("/goal_pose", 10);
+
+
 }
 
 StupidPlanner::~StupidPlanner() {};
 
+void StupidPlanner::velCallback(const geometry_msgs::TwistStamped& msg) {
+	current_vel_ = msg;
+}
+
+void StupidPlanner::poseCallback(const geometry_msgs::PoseStamped& msg) {
+	current_pose_ = msg;
+}
+
 void StupidPlanner::fcuInputGoalCallback(
     const mavros_msgs::Trajectory& msg) {
 
+#ifdef VERBOSE
   ROS_INFO("input p1 pos|vel: %.3f, %.3f, %.3f, %.3f| %.3f, %.3f, %.3f, %.3f",
     msg.point_1.position.x, msg.point_1.position.y, msg.point_1.position.z, msg.point_1.yaw,
      msg.point_1.velocity.x, msg.point_1.velocity.y, msg.point_1.velocity.z, msg.point_1.yaw_rate);
-    ROS_INFO("input p2 pos|vel: %.3f, %.3f, %.3f, %.3f| %.3f, %.3f, %.3f, %.3f",
+  ROS_INFO("input p2 pos|vel: %.3f, %.3f, %.3f, %.3f| %.3f, %.3f, %.3f, %.3f",
     msg.point_2.position.x, msg.point_2.position.y, msg.point_2.position.z, msg.point_2.yaw,
      msg.point_2.velocity.x, msg.point_2.velocity.y, msg.point_2.velocity.z, msg.point_2.yaw_rate);
+#endif
  
-  mavros_msgs::Trajectory obst_free_path = {};
-  transformPoseToObstacleAvoidance(obst_free_path, msg);
-  mavros_obstacle_free_path_pub_.publish(obst_free_path);
+  if(msg.point_valid[1] == true){
+	    goal_pose_.header = msg.header;
+	    goal_pose_.pose.position.x = msg.point_2.position.x;
+	    goal_pose_.pose.position.y = msg.point_2.position.y;
+	    goal_pose_.pose.position.z = msg.point_2.position.z;
+  }
+}
 
-  ROS_INFO("output p1 pos|vel: %.3f, %.3f, %.3f, %.3f| %.3f, %.3f, %.3f, %.3f",
-    obst_free_path.point_1.position.x, obst_free_path.point_1.position.y, 
-    obst_free_path.point_1.position.z, obst_free_path.point_1.yaw,
-    obst_free_path.point_1.velocity.x, obst_free_path.point_1.velocity.y,
-    obst_free_path.point_1.velocity.z, obst_free_path.point_1.yaw_rate);
-  ROS_INFO("output p2 pos|vel: %.3f, %.3f, %.3f, %.3f| %.3f, %.3f, %.3f, %.3f",
-    obst_free_path.point_2.position.x, obst_free_path.point_2.position.y, 
-    obst_free_path.point_2.position.z, obst_free_path.point_2.yaw,
-    obst_free_path.point_2.velocity.x, obst_free_path.point_2.velocity.y,
-    obst_free_path.point_2.velocity.z, obst_free_path.point_2.yaw_rate);
+void StupidPlanner::publishOutput(){
+	// some logic here
+#ifdef POSITION_SETPOINTS
+	double x_dir =0.0d, y_dir = 0.0d, z_dir=CARROT_STICK_LENGTH;
 
-  geometry_msgs::PoseStamped current;
-  geometry_msgs::PoseStamped goal;
-  current.header = msg.header;
-  current.pose.position.x = msg.point_1.position.x;
-  current.pose.position.y = msg.point_1.position.y;
-  current.pose.position.z = msg.point_1.position.z;
-  goal.header = msg.header;
-  goal.pose.position.x = msg.point_2.position.x;
-  goal.pose.position.y = msg.point_2.position.y;
-  goal.pose.position.z = msg.point_2.position.z;
-  current_pos_pub_.publish(current);
-  current_goal_pub_.publish(goal);
+	  // Only go toward goal, if a certain height is reached
+	if(current_pose_.pose.position.z >= CARROT_STICK_LENGTH){
+	     x_dir = (goal_pose_.pose.position.x - current_pose_.pose.position.x);
+	     y_dir = (goal_pose_.pose.position.y - current_pose_.pose.position.y);
+	     z_dir = (goal_pose_.pose.position.z - current_pose_.pose.position.z);
+	    double mag = sqrt(x_dir*x_dir+y_dir*y_dir + z_dir*z_dir);
+	    if(mag >= CARROT_STICK_LENGTH){// only normalize if far away from goal
+	      x_dir /= mag/CARROT_STICK_LENGTH;
+	      y_dir /= mag/CARROT_STICK_LENGTH;
+	      z_dir /= mag/CARROT_STICK_LENGTH;
+	     }
+	}
+
+	// Carrot-and-stick method...
+	obst_free_path_.point_1.position.x = current_pose_.pose.position.x + x_dir;
+	obst_free_path_.point_1.position.y = current_pose_.pose.position.y + y_dir;
+	obst_free_path_.point_1.position.z = current_pose_.pose.position.z + z_dir;
+	obst_free_path_.point_1.velocity.x = NAN;
+	obst_free_path_.point_1.velocity.y = NAN;
+	obst_free_path_.point_1.velocity.z = NAN;
+	obst_free_path_.point_1.yaw = atan2(y_dir,x_dir);
+	obst_free_path_.point_1.yaw_rate = NAN;
+#else
+	obst_free_path_.point_1.position.x = NAN;
+	obst_free_path_.point_1.position.y = NAN;
+	obst_free_path_.point_1.position.z = NAN;
+	obst_free_path_.point_1.velocity.x = x_dir;
+	obst_free_path_.point_1.velocity.y = y_dir;
+	obst_free_path_.point_1.velocity.z = z_dir;
+	obst_free_path_.point_1.yaw = NAN;
+	obst_free_path_.point_1.yaw_rate = msg.point_1.yaw_rate;
+#endif
+
+	obst_free_path_.point_1.acceleration_or_force.x = NAN;
+	obst_free_path_.point_1.acceleration_or_force.y = NAN;
+	obst_free_path_.point_1.acceleration_or_force.z = NAN;
+	fillUnusedTrajectoryPoint(obst_free_path_.point_2);
+	fillUnusedTrajectoryPoint(obst_free_path_.point_3);
+	fillUnusedTrajectoryPoint(obst_free_path_.point_4);
+	fillUnusedTrajectoryPoint(obst_free_path_.point_5);
+	obst_free_path_.time_horizon = {NAN, NAN, NAN, NAN, NAN};
+	obst_free_path_.point_valid = {true, false, false, false, false};
+
+	  mavros_obstacle_free_path_pub_.publish(obst_free_path_);
+	  current_pose_pub_.publish(current_pose_);
+	  goal_pose_pub_.publish(goal_pose_);
+
+#ifdef VERBOSE
+	  ROS_INFO("output p1 pos|vel: %.3f, %.3f, %.3f, %.3f| %.3f, %.3f, %.3f, %.3f",
+			  obst_free_path_.point_1.position.x, obst_free_path_.point_1.position.y,
+			  obst_free_path_.point_1.position.z, obst_free_path_.point_1.yaw,
+			  obst_free_path_.point_1.velocity.x, obst_free_path_.point_1.velocity.y,
+			  obst_free_path_.point_1.velocity.z, obst_free_path_.point_1.yaw_rate);
+	  ROS_INFO("output p2 pos|vel: %.3f, %.3f, %.3f, %.3f| %.3f, %.3f, %.3f, %.3f",
+			  obst_free_path_.point_2.position.x, obst_free_path_.point_2.position.y,
+			  obst_free_path_.point_2.position.z, obst_free_path_.point_2.yaw,
+			  obst_free_path_.point_2.velocity.x, obst_free_path_.point_2.velocity.y,
+			  obst_free_path_.point_2.velocity.z, obst_free_path_.point_2.yaw_rate);
+#endif
+
 }
 
 void StupidPlanner::fillUnusedTrajectoryPoint(
@@ -71,64 +135,18 @@ void StupidPlanner::fillUnusedTrajectoryPoint(
   point.yaw_rate = NAN;
 }
 
-void StupidPlanner::transformPoseToObstacleAvoidance(
-    mavros_msgs::Trajectory& obst_avoid, const mavros_msgs::Trajectory& msg) {
-  obst_avoid.header = msg.header;
-  obst_avoid.type = 0;
-
-
-#ifdef POSITION_SETPOINTS
-  double x_dir =0.0d, y_dir = 0.0d, z_dir=1.0d;
-  // If no goal is defined, just return current position
-if(msg.point_valid[1] == true && msg.point_1.position.z >= 0.5f){
-     x_dir = (msg.point_2.position.x - msg.point_1.position.x);
-     y_dir = (msg.point_2.position.y - msg.point_1.position.y);
-     z_dir = (msg.point_2.position.z - msg.point_1.position.z);
-    double mag = sqrt(x_dir*x_dir+y_dir*y_dir + z_dir*z_dir);
-    if(mag >= 1.0d){// only normalize if far away from goal
-      x_dir /= mag;
-      y_dir /= mag;
-      z_dir /= mag;
-     }
-}
-
-  obst_avoid.point_1.position.x = msg.point_1.position.x + x_dir;
-  obst_avoid.point_1.position.y = msg.point_1.position.y + y_dir;
-  obst_avoid.point_1.position.z = msg.point_1.position.z + z_dir;
-  obst_avoid.point_1.velocity.x = NAN;
-  obst_avoid.point_1.velocity.y = NAN;
-  obst_avoid.point_1.velocity.z = NAN;
-  obst_avoid.point_1.yaw = atan2(y_dir,x_dir);
-  obst_avoid.point_1.yaw_rate = NAN;
-#else
-  obst_avoid.point_1.position.x = NAN;
-  obst_avoid.point_1.position.y = NAN;
-  obst_avoid.point_1.position.z = NAN;
-  obst_avoid.point_1.velocity.x = msg.point_1.velocity.x;
-  obst_avoid.point_1.velocity.y = msg.point_1.velocity.y;
-  obst_avoid.point_1.velocity.z = msg.point_1.velocity.z;
-  obst_avoid.point_1.yaw = NAN;
-  obst_avoid.point_1.yaw_rate = msg.point_1.yaw_rate;
-#endif
-
-  obst_avoid.point_1.acceleration_or_force.x = NAN;
-  obst_avoid.point_1.acceleration_or_force.y = NAN;
-  obst_avoid.point_1.acceleration_or_force.z = NAN;
-
-  fillUnusedTrajectoryPoint(obst_avoid.point_2);
-  fillUnusedTrajectoryPoint(obst_avoid.point_3);
-  fillUnusedTrajectoryPoint(obst_avoid.point_4);
-  fillUnusedTrajectoryPoint(obst_avoid.point_5);
-
-  obst_avoid.time_horizon = {NAN, NAN, NAN, NAN, NAN};
-
-  obst_avoid.point_valid = {true, false, false, false, false};
-}
-
-
 int main(int argc, char** argv) {
   ros::init(argc, argv, "stupid_planner");
   StupidPlanner planner;
+  ros::Rate loop_rate(10.0);
+
+  while (ros::ok())
+  {
+    ros::spinOnce();
+    loop_rate.sleep();
+    planner.publishOutput();
+  }
+
 
   ros::spin();
   return 0;
